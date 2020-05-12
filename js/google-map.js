02be3af470971dea5e10e55c87e5264a45e47c1f
Luis G Luna <luis.lunapa@outlook.com>
2020-05-11 19:23:06 -0500

    Ready home page

--- js/google-map.js
+++ js/google-map.js
@@ -5,13 +5,11 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    var myLatlng = new google.maps.LatLng(20.668886, -103.428284);
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -42,21 +40,28 @@ function init() {
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    myLatlng
 
-        });
-    }
+    new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: 'images/loc.png'
+    });
+    // var addresses = ['New York'];
+    //
+    // for (var x = 0; x < addresses.length; x++) {
+    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+    //         var p = data.results[0].geometry.location
+    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
+    //         new google.maps.Marker({
+    //             position: latlng,
+    //             map: map,
+    //             icon: 'images/loc.png'
+    //         });
+    //
+    //     });
+    // }
     
 }
 google.maps.event.addDomListener(window, 'load', init);
